65e642660f31ebefd10e264d26d770aa10c2622d
Gintaras <combinacijus@gmail.com>
2018-11-02 00:30:30 +0200

    Added displayNum() function, example and did some refractoring
    With displayNum() function now it easy to display negative numbers and floats by passing float or int.
    
    Added example of using displayNum()
    
    Changed coding() function to be cleaner. Removed duplicate code.
    
    Formatted code to look better.
    
    Added comments on how code works.
    
    Renamed some variables to more conventional names.

--- TM1637.h
+++ TM1637.h
@@ -1,6 +1,9 @@
 /*
  * TM1637.h
  * A library for the 4 digit display
+ * 
+ * TM1637 chip datasheet:
+ * https://www.mcielectronics.cl/website_MCI/static/documents/Datasheet_TM1637.pdf
  *
  * Copyright (c) 2012 seeed technology inc.
  * Website    : www.seeed.cc
@@ -33,41 +36,44 @@
 #define TM1637_h
 #include <inttypes.h>
 #include <Arduino.h>
-//************definitions for TM1637*********************
-#define ADDR_AUTO  0x40
+/*******************Definitions for TM1637*********************/
+#define ADDR_AUTO 0x40
 #define ADDR_FIXED 0x44
 
-#define STARTADDR  0xc0
-/**** definitions for the clock point of the digit tube *******/
-#define POINT_ON   1
-#define POINT_OFF  0
-/**************definitions for brightness***********************/
-#define  BRIGHT_DARKEST 0
-#define  BRIGHT_TYPICAL 2
-#define  BRIGHTEST      7
+#define STARTADDR 0xc0
+/*****Definitions for the clock point of the digit tube *******/
+#define POINT_ON 1
+#define POINT_OFF 0
+/**************Definitions for brightness**********************/
+#define BRIGHT_DARKEST 0
+#define BRIGHT_TYPICAL 2
+#define BRIGHTEST 7
 
 class TM1637
 {
-  public:
-    uint8_t Cmd_SetData;
-    uint8_t Cmd_SetAddr;
-    uint8_t Cmd_DispCtrl;
-    boolean _PointFlag;     //_PointFlag=1:the clock point on
-    TM1637(uint8_t, uint8_t);
-    void init(void);        //To clear the display
-    int  writeByte(int8_t wr_data);//write 8bit data to tm1637
-    void start(void);//send start bits
-    void stop(void); //send stop bits
-    void display(int8_t DispData[]);
-    void display(uint8_t BitAddr,int8_t DispData);
-    void clearDisplay(void);
-    void set(uint8_t = BRIGHT_TYPICAL,uint8_t = 0x40,uint8_t = 0xc0);//To take effect the next time it displays.
-    void point(boolean PointFlag);//whether to light the clock point ":".To take effect the next time it displays.
-    void coding(int8_t DispData[]);
-    int8_t coding(int8_t DispData);
-    void bitDelay(void);
-  private:
-    uint8_t Clkpin;
-    uint8_t Datapin;
+public:
+  uint8_t cmd_set_data;
+  uint8_t cmd_set_addr;
+  uint8_t cmd_disp_ctrl;
+  boolean _PointFlag;            //_PointFlag=1:the clock point on
+  TM1637(uint8_t, uint8_t);
+  void init(void);               // To clear the display
+  int writeByte(int8_t wr_data); // Write 8bit data to tm1637
+  void start(void);              // Send start bits
+  void stop(void);               // Send stop bits
+  void display(int8_t DispData[]);
+  void display(uint8_t BitAddr, int8_t DispData);
+  void displayNum(float num, int decimal = 0, bool show_minus = true);
+  void clearDisplay(void);
+  void set(uint8_t = BRIGHT_TYPICAL, uint8_t = 0x40, uint8_t = 0xc0); //To take effect the next time it displays.
+  void point(boolean PointFlag);                                      //whether to light the clock point ":".To take effect the next time it displays.
+  void coding(int8_t DispData[]);
+  int8_t coding(int8_t DispData);
+  void bitDelay(void);
+
+private:
+  const int DIGITS = 4; // Number of digits on display
+  uint8_t clkpin;
+  uint8_t datapin;
 };
 #endif
---
+++ examples/DisplayNum/DisplayNum.ino
@@ -0,0 +1,80 @@
+/*
+ * TM1637.cpp
+ * A library for the 4 digit display
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Frankie.Chu
+ * Create Time: 9 April,2012
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "TM1637.h"
+
+// Pins definitions for TM1637 and can be changed to other ports
+const int CLK = 6;
+const int DIO = 7;
+TM1637 tm1637(CLK,DIO);
+
+float number = -1234.5;
+int mult = 1;
+
+void setup()
+{
+  Serial.begin(9600);
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL);//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+
+  float num = -123.45;
+
+  // Without specifying decimal pointt it displays int
+  tm1637.displayNum(num);    // -123
+  delay(2000);
+
+  // When numbers doesn't fit biggest digits are truncated
+  tm1637.displayNum(num, 1); // -234
+  delay(2000);
+  
+  // Colon light up as decimal point
+  tm1637.displayNum(num, 2);
+  delay(2000);
+  
+  // Note that adding 3 digits (-123450) makes number out of int range
+  tm1637.displayNum(num, 3);
+  delay(2000);
+
+  // Display negative numbers without sign
+  tm1637.displayNum(num, 0, false);
+  delay(2000);
+}
+
+void loop()
+{
+  tm1637.displayNum(number);
+  
+  number += 17.42 * mult;
+  if (abs(number) > 1500)
+    mult *= -1;
+  
+  delay(50);
+}
